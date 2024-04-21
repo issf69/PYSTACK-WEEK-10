4ca373e490ac7614dfbbd23c6f3c32087c7aa375
Irene <irenesilvafranca2016@gmail.com>
2024-04-21 19:58:14 -0300

    PYSTACK WEEK 10 -Aula_1/ Aula_2 / Aula_3  /2024

--- paciente/views.py
+++ paciente/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
-from medico.models import DadosMedico, Especialidades, DatasAbertas
+from medico.models import DadosMedico, Especialidades, DatasAbertas, is_medico
 from django.http import HttpResponse
 from datetime import datetime
-from .models import Consulta
+from .models import Consulta, Documento
 from django.contrib import messages
 from django.contrib.messages import constants
 # Create your views here.
@@ -19,13 +19,13 @@ def home(request):
         if especialidades_filtrar:
             medicos = medicos.filter(especialidade_id__in=especialidades_filtrar)
         especialidades = Especialidades.objects.all()
-        return render(request, 'home.html', {'medicos': medicos, 'especialidades': especialidades})
+        return render(request, 'home.html', {'medicos': medicos, 'especialidades': especialidades, 'is_medico': is_medico(request.user)})
 
 def escolher_horario(request, id_dados_medicos):
     if request.method == "GET":
         medico = DadosMedico.objects.get(id=id_dados_medicos)
         datas_abertas = DatasAbertas.objects.filter(user=medico.user).filter(data__gte=datetime.now()).filter(agendado=False)
-        return render(request, 'escolher_horario.html', {'medico': medico, 'datas_abertas': datas_abertas})
+        return render(request, 'escolher_horario.html', {'medico': medico, 'datas_abertas': datas_abertas, 'is_medico': is_medico(request.user)})
 
 
 def agendar_horario(request, id_data_aberta):
@@ -52,4 +52,16 @@ def minhas_consultas(request):
     if request.method == "GET":
         #TODO: desenvolver filtros
         minhas_consultas = Consulta.objects.filter(paciente=request.user).filter(data_aberta__data__gte=datetime.now())
-        return render(request, 'minhas_consultas.html', {'minhas_consultas': minhas_consultas})
+        return render(request, 'minhas_consultas.html', {'minhas_consultas': minhas_consultas, 'is_medico': is_medico(request.user)})
+
+
+def consulta(request, id_consulta):
+    if request.method == 'GET':
+        consulta = Consulta.objects.get(id=id_consulta)
+        dado_medico = DadosMedico.objects.get(user=consulta.data_aberta.user)
+        documento = Documento.objects.filter(consulta=consulta)
+        return render(request, 'consulta.html', {'consulta': consulta, 'dado_medico': dado_medico, 'documentos': documentos})
+
+# Fazer a validação de segurança nos restantes pontos do código
+# Botão de cancelar consulta
+# Dashboard
